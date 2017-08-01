310895356d6e292f35eeb715a6a95f0531569c45
victor <contatos.caldas@gmail.com>
2017-08-01 18:20:11 -0300

    Not Fix error in DataBinding

---
+++ app/src/main/java/com/mastodon/TestingandDebugging/BindingDataActivity.java
@@ -0,0 +1,19 @@
+package com.mastodon.TestingandDebugging;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import static com.mastodon.R.*;
+
+public class BindingDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_binding_data);
+
+
+    }
+}
